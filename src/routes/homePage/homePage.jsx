fbfdb11a7203b5f010eab6b4c6da5f438884f7b8
honey hauwa <hauwa1602@gmail.com>
2025-04-07 23:01:46 +0100

    Refactor homePage component structure

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,16 @@
+import './homePage.scss'
+
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <div className="textContainer">text</div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+    
+  )
+}
+
+export default HomePage

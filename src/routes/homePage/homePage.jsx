5401076d006f2221728b4f2be32eefcdf665f662
honey hauwa <hauwa1602@gmail.com>
2025-04-11 15:38:34 +0100

    home page

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,13 +1,39 @@
 import './homePage.scss'
+import SearchBar from '../../components/searchBar/SearchBar'
 
 
 function HomePage() {
   return (
-    <div className="homepage">
-      <div className="textContainer">text</div>
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+        <h1 className='title'>Find Real Estate & Get Your Dream Place</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur vitae facere 
+          velit eum perferendis ad, pariatur magnam facilis sequi quo deserunt.
+           Earum, et maiores. Deserunt, enim. Inventore consequatur quisquam obcaecati.
+         corrupti voluptate. Corrupti iusto neque incidunt tempora vel sit reprehenderit et,
+          rem ipsa soluta magni culpa obcaecati mollitia, deserunt dignissimos?</p>
+          <SearchBar/>
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000</h1>
+              <h2>Property Ready</h2>
+            </div>
+            
+          </div>
+        </div>
+      </div>
       <div className="imgContainer">
         <img src="/bg.png" alt="" />
-      </div>
+        </div>
     </div>
     
   )

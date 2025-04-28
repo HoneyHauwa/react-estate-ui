4eef52e73af3a1edea7abf38b9d90a4e8e04f9c5
honey hauwa <hauwa1602@gmail.com>
2025-04-28 15:35:58 +0100

    listPage

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,8 +1,40 @@
-import './SearchBar.scss'
+import { useState } from 'react';
+import './SearchBar.scss';
+
+const type = ["buy","rent"]
 
 function SearchBar(){
+    const [query, setQuery] =useState({
+        type:"buy",
+        location:"",
+        minPrice:0,
+        maxPrice:0,
+    })
+
+    const switchType = (val)=>{
+        setQuery(prev=>({...prev,type:val}))
+
+    };
+
     return (
-        <div className="searchBar">SearchBar</div>
+        <div className="searchBar">
+            <div className="type">
+                {type.map(type=>(
+                    <button key={type} onClick={()=>switchType(type)} className={query.type === type ? "active" : "" }>
+                        {type}
+                        </button>
+                ))}
+                
+            </div>
+            <form action="">
+                <input type="text" name='location' placeholder='City Location' />
+                <input type="text" number='minPrice' min={0} max={10000000} placeholder='Min Price' />
+                <input type="text" number='maxPrice' min={0} max={10000000} placeholder='Max Price' />
+                <button>
+                    <img src="/search.png" alt="" />
+                </button>
+            </form>
+        </div>
     )
 }
 
